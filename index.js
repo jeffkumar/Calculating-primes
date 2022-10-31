d8641519ac1099a502b8a53a8590279e12710b89
jeffkumar <jeffkumar.aw@gmail.com>
2022-10-30 23:09:48 -0400

    3 files modified

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 // Import stylesheets
 import './style.css';
 import { calculatePrimes } from './primes.js';
-
+const cp = calculatePrimes.toString().replace(/^function .+\{?|\}$/g, '');
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 
-const ps = calculatePrimes.toString().replace(/^function .+\{?|\}$/g, '');
-
-const workerBlob = new Blob([ps], { type: 'text/javascript' });
+const workerBlob = new Blob([cp], { type: 'text/javascript' });
 const url = URL.createObjectURL(workerBlob);
 const worker = new Worker(url);
 
+let start = Date.now();
+let interval = setInterval(() => {
+  document.getElementById('time').innerHTML = (Date.now() - start) / 1000;
+}, 1);
+
 worker.addEventListener('message', (e) => {
-  appDiv.innerHTML = e.data.map((n) => ' ' + n);
+  appDiv.innerHTML = 'Painting...';
+  setTimeout(() => (appDiv.innerHTML = e.data.map((n) => ' ' + n)), 100);
+  clearInterval(interval);
 });
 
 worker.postMessage({ msg: 'start', limit: 20000000 });
